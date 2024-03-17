40f098ff232ae54266b520065e9b03975a0c81b3
Law Wai Chun <chunlaw@rocketmail.com>
2024-03-17 15:25:08 +0800

    Export constants value

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import AneHk from "./AneHk";
 
 export default AneHk;
 
+export { HOSPITAL_GEOCOOR, HOSPITAL_MAP, AVAILABLE_HOSPITALS } from "./constants"
+
 export type { DayTimePoint, Hospital } from "./types"
